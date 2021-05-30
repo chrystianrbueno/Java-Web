c3931eadc420b9ccede7b44fedbbb0ec00e8674f
Chrystian Rocha <chrystianrbueno@gmail.com>
2021-05-29 23:21:49 -0300

    pagina professores finalizada

--- front-end/controller/professores.js
+++ front-end/controller/professores.js
@@ -7,4 +7,21 @@ professoresModulo.controller("professoresController", function ($scope){
 		{codigo: 2, nome: 'Juka', email: 'Juka@gmail.com', fone: '777777777'},
 		{codigo: 3, nome: 'Adalberto', email: 'Adalberto@gmail.com', fone: '888888888'},
 	];
+	
+	$scope.selecionaProfessor = function(professorSelecionado){
+		$scope.professor = professorSelecionado;
+	}
+	
+	$scope.limparCampos = function(){
+		$scope.professor = null;
+	}
+	
+	$scope.salvar = function(){
+		$scope.professores.push($scope.professor);
+		$scope.limparCampos();
+	}
+	
+	$scope.excluir = function(){
+		$scope.professores.splice($scope.professores.indexOf($scope.professor),1);
+	}
 });
